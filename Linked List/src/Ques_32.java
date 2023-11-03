ca42203341db4a9dbe3d2d6539a5b5c901d90458
bharatAmeria <the.bharat.ameria@gmail.com>
2023-11-03 09:43:45 +0530

    Ques 35

--- Linked List/src/Ques_32.java
+++ Linked List/src/Ques_32.java
@@ -1,22 +1,31 @@
 // Sort linked list containing 0’s, 1’s, and 2’s in a single traversal
+class Node0{
+    int data;
+    Node0 next;
+    Node0(int data, Node0 next) {
+        this.data = data;
+        this.next = next;
+    }
 
+    Node0() {}
+}
 public class Ques_32 {
-    public static void printList(Node1 head) {
-        Node1 ptr = head;
+    public static void printList(Node0 head) {
+        Node0 ptr = head;
         while (ptr != null) {
             System.out.println(ptr.data + " -> ");
             ptr = ptr.next;
         }
         System.out.println("null");
     }
-    public static Node1 sortList(Node1 head) {
+    public static Node0 sortList(Node0 head) {
         if (head == null || head.next == null) {
             return head;
         }
-        Node1 first = new Node1(), second = new Node1(), third = new Node1();
-        Node1 zero = first, one = second, two = third;
+        Node0 first = new Node0(), second = new Node0(), third = new Node0();
+        Node0 zero = first, one = second, two = third;
 
-        Node1 curr = head;
+        Node0 curr = head;
         while (curr != null) {
 
             if (curr.data == 0) {
@@ -39,9 +48,9 @@ public class Ques_32 {
 
     public static void main(String[] args) {
         int[] keys = {1, 2, 0, 0, 1, 2, 1, 2, 1};
-        Node1 head = null;
+        Node0 head = null;
         for (int i = keys.length - 1; i >= 0; i--) {
-            head = new Node1(keys[i], head);
+            head = new Node0(keys[i], head);
         }
         head = sortList(head);
         printList(head);
--- Linked List/src/Ques_35.java
+++ Linked List/src/Ques_35.java
@@ -1,18 +1,30 @@
 import java.util.HashSet;
 import java.util.Set;
 
+class Node9
+{
+    int data;
+    Node9 next;
+
+    Node9(int data, Node9 next)
+    {
+        this.data = data;
+        this.next = next;
+    }
+}
+
 public class Ques_35 {
-        public static void printList(Node head) {
-            Node ptr = head;
+        public static void printList(Node9 head) {
+            Node9 ptr = head;
             while (ptr != null) {
                 System.out.println(ptr.data + " -> ");
                 ptr = ptr.next;
             }
             System.out.println("null");
         }
-        public static Node removeDuplicate(Node head) {
-            Node previous = null;
-            Node current = head;
+        public static void removeDuplicate(Node9 head) {
+            Node9 previous = null;
+            Node9 current = head;
 
             Set<Integer> set = new HashSet<>();
 
@@ -26,14 +38,13 @@ public class Ques_35 {
                 }
                 current= previous.next;
             }
-            return head;
         }
 
         public static void main(String[] args) {
             int[] keys = {5,3,4,2,5,4,1,3};
-            Node head = null;
+            Node9 head = null;
             for (int i = keys.length - 1; i >0 ; i--) {
-                head = new Node(keys[i], head);
+                head = new Node9(keys[i], head);
             }
             removeDuplicate(head);
             printList(head);
